b75f172b3cc766199421792c693b031b461e3a86
neelrahulr <rathorerahul131@gmail.com>
2020-11-26 18:59:51 +0530

    Refactored: Validated for @

--- emailValidation.sh
+++ emailValidation.sh
@@ -7,7 +7,7 @@ read -p "Enter your email address : " email
 
 #abc.xyz@BridgeLabz.co.in
 #Required regex for email
-reg_pat="^[a-zA-Z0-9]+$"
+reg_pat="^[a-zA-Z0-9]*@[a-zA-Z0-9]+$"
 
 if [[ $email =~ $reg_pat ]]
 then
