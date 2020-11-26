6618561d81d92528b68b6adeeabdefbe7cae8c64
neelrahulr <rathorerahul131@gmail.com>
2020-11-26 18:52:17 +0530

    Refactored: Validated the Start of MailId

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash/
 
 echo "Welcome to Email Address Validation System"
+
+read -p "Enter your email address : " email
+
+
+#abc.xyz@BridgeLabz.co.in
+#Required regex for email
+reg_pat="^[a-zA-Z0-9]+$"
+
+if [[ $email =~ $reg_pat ]]
+then
+        echo "Your Email Id $email is Valid"
+else
+        echo "Invalid! Please enter Valid ID"
+fi
